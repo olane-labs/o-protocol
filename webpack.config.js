7bcc69353a81d5c9e72aeeaa44b052fb628dfd00
Brendon Ghaderi <3301521+brendong@users.noreply.github.com>
2025-06-18 18:53:04 -0700

    added aggregate function

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: [
+          {
+            loader: 'ts-loader',
+            options: {
+              compilerOptions: {
+                removeComments: false, // Preserve comments
+                declaration: false, // Don't generate .d.ts files
+                outDir: undefined, // Don't output to separate files
+              }
+            }
+          },
+          {
+            loader: path.resolve(__dirname, 'ts-aggregator-loader.js'),
+          }
+        ],
+        exclude: /node_modules|index.ts/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  output: {
+    filename: 'aggregated.ts',
+    path: path.resolve(__dirname, 'dist'),
+    
+  },
+  mode: 'development',
+}; 
